f1e22715d8ee53c2835770344d3be6aec83da9df
nisol91 <nicolasolzi91@gmail.com>
2018-12-17 18:28:04 +0100

    3 [chiamata ajax]

--- main.js
+++ main.js
@@ -2,8 +2,52 @@ $(document).ready(function() {
 
 
 $('.my_button').click(function() {
-  $('.searchbar input').val('')//corretto?
+  //pulisco quello che c era scritto nella ricerca
+  // $('.searchbar input').val('')
+
+  var ricerca = $('.searchbar .my_input').val()
+  console.log(ricerca);
+
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/search/movie?api_key=e1cd6fed3cf1a6213a3fd2941b25d0fc',
+    type: 'GET',
+    data: {
+      // key: 'e1cd6fed3cf1a6213a3fd2941b25d0fc',
+      query: ricerca,
+    },
+    success: function(data) {
+    console.log("success");
+
+
+    var risultato = data
+    // console.log(risultato);
+    var films = data.results
+    console.log(films);
+
+    for (var i = 0; i < films.length; i++) {
+      var copy = $('.templates .film').clone();
+      $('.vetrina').append(copy)
+    }
+    $('.vetrina .film').each(function(index) {
+      $(this).find('h1').text(films[index]['title'])
+      $(this).find('h2').text(films[index]['original_title'])
+      $(this).find('h3').text(films[index]['vote_average'])
+      $(this).find('h4').text(films[index]['original_language'])
+
+    });
+
+    },
+    error: function() {
+    console.log("error");
+    }
+  })
+
+
+
+
+
 });
+
 
 
 

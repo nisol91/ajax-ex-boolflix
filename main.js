3fb95b09ce57c0689868538674819b4d0d3bd4b9
nisol91 <nicolasolzi91@gmail.com>
2018-12-18 11:28:55 +0100

    5 [flags]

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+//lingue
+
+var languages = [
+  {
+    lang: 'en',
+    img: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1920px-Flag_of_the_United_Kingdom.svg.png'
+  },
+]
+
 //funzione principale
 function my_query(ricerca) {
 
@@ -8,32 +17,39 @@ function my_query(ricerca) {
     url: 'https://api.themoviedb.org/3/search/movie?api_key=e1cd6fed3cf1a6213a3fd2941b25d0fc',
     type: 'GET',
     data: {
-      // key: 'e1cd6fed3cf1a6213a3fd2941b25d0fc',
       query: ricerca,
     },
     success: function(data) {
       console.log("success");
-
 
       var risultato = data
       // console.log(risultato);
       var films = data.results
       console.log(films);
 
+      //creo un numero di cartoline uguale al numero di film trovati dal DB
       for (var i = 0; i < films.length; i++) {
         var copy = $('.templates .film').clone();
         $('.vetrina').append(copy)
       }
+
+      //inserisco tutti i valori del film presi dal DB
       $('.vetrina .film').each(function(index) {
         var voto_10 = films[index]['vote_average']
         var voto_5 = Math.floor(voto_10/2)
 
-
+        var lingua = films[index]['original_language']
+        for (var i = 0; i < languages.length; i++) {
+          console.log(languages[i].lang);
+          if (lingua === languages[i].lang) {
+            $(this).find('.lingua img').attr('src', languages[i].img);
+          }
+        }
 
         $(this).find('h1').text(films[index]['title'])
         $(this).find('h2').text(films[index]['original_title'])
         $(this).find('h3').text(voto_5)
-        $(this).find('h4').text(films[index]['original_language'])
+        $(this).find('h4').text(lingua)
         for (var i = 0; i < voto_5; i++) {
           var copy_star = $('.templates .stelle_piene').clone();
           $(this).find('.stars').append(copy_star)
